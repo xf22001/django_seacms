c6779d16e304d07037d8cf53201f56eb00c35a56
xiaofei <alibaba22001@163.com>
2021-01-05 12:30:47 +0800

    update

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Register your models here.
+from django.apps import apps
+from django.contrib.admin.sites import AlreadyRegistered #获取app:api下所有的model得到一个生成器
+
+app_models = apps.get_app_config('app').get_models()
+for model in app_models:
+    admin.site.register(model)
